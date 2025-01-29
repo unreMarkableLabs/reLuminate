93c7e679a77fed5f78b4234106144ae4e7658df8
Stephen Papierski <stephenpapierski@users.noreply.github.com>
2025-01-29 09:37:22 -0700

    Remove need for wget in install-reLuminate.sh
    Since wget doesn't support https, generate reLuminate.service instead of trying to download it.

--- install-reLuminate.sh
+++ install-reLuminate.sh
@@ -3,18 +3,11 @@
 
 # --- Values replaced in github actions ---
 version='VERSION'
-service_file_sha256sum='SERVICE_FILE_SHA256SUM'
 # -----------------------------------------
 
 installfile='./install-reLuminate.sh'
 pkgname='reLuminate'
 servicefile="/lib/systemd/system/${pkgname}.service"
-
-# TODO this needs updated to an arm64 version of wget (https://github.com/rM-self-serve/webinterface-onboot/issues/6)
-# OR...it looks like the reMarkable Paper Pro might already have a wget version that works without this (to be verified)
-wget_path=/home/root/.local/share/reLuminate/wget
-wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
-wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
 
 main() {
   case "$@" in
@@ -40,58 +33,30 @@ cli_info() {
 	echo ''
 }
 
-srvc_sha_check() {
-	sha256sum -c <(echo "$service_file_sha256sum  $servicefile") >/dev/null 2>&1
-}
-
-sha_fail() {
-	echo "sha256sum did not pass, error downloading ${pkgname}"
-	echo "Exiting installer and removing installed files"
-	[[ -f $servicefile ]] && rm $servicefile
-	exit 1
-}
-
 install() {
 	echo "Install ${pkgname} ${version}"
 	echo ''
 
-  if [ -f "$wget_path" ] && ! sha256sum -c <(echo "$wget_checksum  $wget_path") >/dev/null 2>&1; then
-		rm "$wget_path"
-  fi
-  if ! [ -f "$wget_path" ]; then
-		echo "Fetching secure wget"
-		# Download and compare to hash
-		mkdir -p "$(dirname "$wget_path")"
-		if ! wget -q "$wget_remote" --output-document "$wget_path"; then
-			echo "Error: Could not fetch wget, make sure you have a stable Wi-Fi connection"
-			exit 1
-		fi
-	fi
-	if ! sha256sum -c <(echo "$wget_checksum  $wget_path") >/dev/null 2>&1; then
-		echo "Error: Invalid checksum for the local wget binary"
-		exit 1
-	fi
-	chmod 755 "$wget_path"
+	# Make file system writeable
+	umount -l /etc
+	mount -o remount,rw /
 
-	need_service=true
-	if [ -f $servicefile ]; then
-		if srvc_sha_check; then
-			need_service=false
-			echo "Already have the right version of ${pkgname}.service"
-		else
-			rm $servicefile
-		fi
-	fi
-	if [ "$need_service" = true ]; then
-    "$wget_path" -q "https://github.com/unreMarkableLabs/${pkgname}/releases/download/${version}/${pkgname}.service" \
-			-O "$servicefile"
+	# Create reLuminate systemd service file
+	cat <<EOF > $servicefile
+	[Unit]
+	Description=Enable linear_mapping for reading light
+	After=multi-user.target
 
-		if ! srvc_sha_check; then
-			sha_fail
-		fi
+	[Service]
+	Type=oneshot
+	ExecStart=/bin/sh -c 'echo yes > /sys/class/backlight/rm_frontlight/linear_mapping'
+	ExecStartPost=/bin/sh -c 'cat /sys/class/backlight/rm_frontlight/actual_brightness > /sys/class/backlight/rm_frontlight/brightness'
+	ExecStop=/bin/sh -c 'echo no > /sys/class/backlight/rm_frontlight/linear_mapping'
+	ExecStopPost=/bin/sh -c 'echo 260 > /sys/class/backlight/rm_frontlight/brightness'
 
-		echo "Fetched ${pkgname}.service"
-	fi
+	[Install]
+	WantedBy=multi-user.target
+	<EOF>
 
 	# Start service
  	systemctl daemon-reload
